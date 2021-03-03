aec6af20455404b6de682c21cf969493cf7289c4
Gerald <578634482yqh@gmail.com>
2021-03-03 22:58:31 +0800

    add logging filter

--- src/main/java/com/test/application/config/HttpTraceConfiguration.java
+++ src/main/java/com/test/application/config/HttpTraceConfiguration.java
@@ -2,9 +2,22 @@ package com.test.application.config;
 
 import org.springframework.boot.actuate.audit.InMemoryAuditEventRepository;
 import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @Configuration
 @Import(value = {InMemoryHttpTraceRepository.class, InMemoryAuditEventRepository.class})
-public class HttpTraceConfiguration {}
+public class HttpTraceConfiguration {
+
+  @Bean
+  public CommonsRequestLoggingFilter loggingFilter() {
+      CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
+      filter.setIncludeQueryString(true);
+      filter.setIncludePayload(true);
+      filter.setMaxPayloadLength(10000);
+      filter.setIncludeClientInfo(true);
+    return filter;
+  }
+}
